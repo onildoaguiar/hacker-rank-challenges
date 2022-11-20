4cb8cc8a350d4af7a099c9bd3bc7e7f7d6f39387
Onildo Aguiar <onildo.aguiar@live.com>
2022-11-20 01:34:36 -0500

    feat: left rotation challenge

---
+++ data-structures/arrays/left-rotation/golang/left_rotation.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+ * Complete the 'rotateLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+func rotateLeft(d int32, arr []int32) []int32 {
+    var count = d
+    var rotatedArr = arr
+    var arrLenght = len(rotatedArr)
+    
+    for ok := true; ok; ok = ( count > 0) { 
+        var tempArr = make([]int32, arrLenght)
+        var first int32
+        
+        for i := 0; i < arrLenght; i++ {
+            if i == 0 {
+                first = rotatedArr[i]
+            } else {
+                tempArr[i-1] = rotatedArr[i]
+            }
+        }
+        tempArr[arrLenght-1] = first
+        rotatedArr = tempArr
+    
+        count -= 1;
+    }  
+    
+    return rotatedArr
+}
+
+func main() {
+    intputArr := []int32 {1, 2, 3, 4, 5}
+    var inputD int32 = 54 
+
+    outputArr := rotateLeft(inputD, intputArr)
+    fmt.Printf("outputArr: %v, %T\n", outputArr, outputArr)
+}
